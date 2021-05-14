510c1ce00aaadb81ebfaec208dbc5b169b200bc7
ErrorGamer2000 <>
2021-05-14 19:47:08 +0000

    Update project api

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const Scratch = require("./api.js");
 (async function () {
   const s = await Scratch.UserSession.create();
   await s.verify();
-  let p = await s.getUserProjects();
+  let p = await s.projects.getUserProjects();
   console.log(p[0]);
 })();
 
