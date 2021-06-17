674691c368a284d27a84972aa0b2a50cbc2da399
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-06-16 17:00:40 -0700

    stuff

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const Scratch = require("./api.js");
 
 (async function () {
-  let user = await Scratch.API.users.getFollowers("ErrorGamer2000");
-  console.log(user.map((v) => v.username));
+  let user = await Scratch.API.users.get("ErrorGamer2000-Bot");
+  console.log(user);
 })();
 
 ("");
